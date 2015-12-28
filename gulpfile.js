b6da70e469296fb3a60271c249fd8ed9ff849463
Daniel Gómez Didier <dnlgmzddr@gmail.com>
2015-12-28 18:06:39 -0500

    GulpJS complete.
    * Added fully functional gulpjs.
    * FAIL to serve static files @ dev time (django dev server).

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-var gulp = require("gulp"),
-    gutil = require("gulp-util"),
-    concat = require("gulp-concat"),
-    mainBowerFiles = require("main-bower-files"),
-    uglify = require("gulp-uglify");
-
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var concat = require("gulp-concat");
+var mainBowerFiles = require("main-bower-files");
+var uglify = require("gulp-uglify");
+var streamqueue = require('streamqueue');
+var nano = require('gulp-cssnano');
+var less = require("gulp-less");
 
 var debug = false;
+var outputRoot = "static/";
 
-gulp.task("js", function() {
+
+gulp.task("lib_js", function () {
     debug = debug || false;
 
     var plugins = mainBowerFiles("**/*.js");
-    if(debug) gutil.log(plugins);
+    if (debug) gutil.log(plugins);
 
     var src = gulp.src(plugins);
-    if(!debug) src = src.pipe(uglify());
+    if (!debug) src = src.pipe(uglify());
 
     return src
         .pipe(concat("lib.js"))
-        .pipe(gulp.dest("static/dist/"));
+        .pipe(gulp.dest(outputRoot+"dist/"));
 });
 
-gulp.task("css", function() {
+gulp.task("lib_css", function () {
     debug = debug || false;
-    var plugins = mainBowerFiles("**/*.css");
-    if(debug) gutil.log(plugins);
-
-    var src = gulp.src(plugins);
-    if(!debug) src = src.pipe(uglify());
-
-    return src
-        .pipe(concat("lib.css"))
-        .pipe(gulp.dest("static/dist/"));
+    return streamqueue({objectMode: true},
+        gulp.src(mainBowerFiles("**/*.less")).pipe(less()),
+        gulp.src(mainBowerFiles("**/*.css")))
+        .pipe(concat('lib.css')).pipe(nano()).pipe(gulp.dest(outputRoot+"dist/"))
 });
 
-gulp.task("default", ["js","css"]);
 
-gulp.task("debug", function() {
+gulp.task("default", ["lib_js", "lib_css"]);
+
+gulp.task("debug", function () {
     debug = true;
     gutil.log(gutil.colors.green('RUNNING IN DEBUG MODE'));
     gulp.start('default');
